86cb63f2c0da63628575de6be8b8a2550e6144c9
Oliver Borders <oliver.r.borders@gmail.com>
2024-11-21 16:08:45 +0000

    First upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#DA291C',
+        'cream': '#FFF7F3',
+      }
+    },
+  },
+  plugins: [],
+}
