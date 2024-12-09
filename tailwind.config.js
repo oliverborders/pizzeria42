0e83cf49cb852e8193af9e2118f74ca2903c17d4
Oliver Borders <oliver.r.borders@gmail.com>
2024-12-09 16:56:04 +0000

    Adding KE and OD buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
       colors: {
         'primary': '#DA291C',
         'cream': '#FFF2E5',
+        'cream-dark': '#FFE0C1',
+        'bodycopy': '#333333',
+        'keprimary': '#F7A617',
+        'kesecondary': '#F8B644',
+        'odprimary': '#E41919',
+        'odsecondary': '#D81818',
       }
     },
   },
